b17b3dd77651555a5f42ec87edd6591577dd98cc
ToshitaC <toshitachaudhari17@gmail.com>
2021-09-28 16:55:27 +0530

    Added:Program for calculate fare

--- src/main/java/com/InvoiceGenerator/CabInvoiceGenerator.java
+++
@@ -1,7 +0,0 @@
-package com.InvoiceGenerator;
-
-public class CabInvoiceGenerator {
-    public static void main(String[] args) {
-        System.out.println("**Welcome Message for Day3 Program**");
-    }
-}
---
+++ src/main/java/com/InvoiceGenerator/InvoiceGeneratorMain.java
@@ -0,0 +1,7 @@
+package com.InvoiceGenerator;
+
+public class InvoiceGeneratorMain {
+    public static void main(String[] args) {
+        System.out.println("**Welcome message for Day30 Program**");
+    }
+}

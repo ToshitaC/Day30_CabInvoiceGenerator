98505810e2ae32665a95c807efa0f10362ed28b0
ToshitaC <toshitachaudhari17@gmail.com>
2021-09-28 17:10:01 +0530

    Modified:Program for no of rides,total,Avg fare per ride

--- src/main/java/com/InvoiceGenerator/InvoiceGeneratorTest.java
+++ src/main/java/com/InvoiceGenerator/InvoiceGeneratorTest.java
@@ -13,6 +13,7 @@ public class InvoiceGeneratorTest {
         double fare = invoiceGenerator.calculateFare(distance, time);
         Assert.assertEquals(25, fare, 0.0);
     }
+
     @Test
     public void givenLessDistanceAndTime_ShouldReturnMinFare() {
 
@@ -23,11 +24,22 @@ public class InvoiceGeneratorTest {
     }
 
     @Test
-    public void givenMultipleRides_ShouldReturnInvoiceSummary() {
+    public void givenMultipleRides_ShouldReturnTotalFare() {
 
         Ride[] rides = {new Ride(2.0, 5),
                 new Ride(0.1, 1)};
         double totalFare = invoiceGenerator.calculateFare(rides);
         Assert.assertEquals(30, totalFare, 0.0);
+    }
+
+    @Test
+    public void givenMultipleRides_ShouldReturnInvoiceSummary() {
+
+        Ride[] rides = {new Ride(2.0, 5),
+                new Ride(0.1, 1)};
+        InvoiceSummary summary = invoiceGenerator.calculateFareReturnObject(rides);
+        InvoiceSummary expectedSummary = new InvoiceSummary(2, 30);
+        if (expectedSummary.getAverageFare() == summary.getAverageFare() && expectedSummary.getNumberOfRides() == summary.getNumberOfRides() && expectedSummary.getTotalFare() == summary.getTotalFare())
+            Assert.assertEquals(1, 1);
     }
 }

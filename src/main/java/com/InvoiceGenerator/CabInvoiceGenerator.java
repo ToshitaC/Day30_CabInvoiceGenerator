320d9fea43e77f38b2f442993c10e5a67edd6fec
ToshitaC <toshitachaudhari17@gmail.com>
2021-09-28 16:27:05 +0530

    Added:Welcome message

---
+++ src/main/java/com/InvoiceGenerator/CabInvoiceGenerator.java
@@ -0,0 +1,7 @@
+package com.InvoiceGenerator;
+
+public class CabInvoiceGenerator {
+    public static void main(String[] args) {
+        System.out.println("**Welcome Message for Day3 Program**");
+    }
+}

d3257a20325417d6f2f4560103a17269b4cb0cee
ToshitaC <toshitachaudhari17@gmail.com>
2021-09-28 17:02:45 +0530

    Added:Program for calculating fare for multiple rides

--- src/main/java/com/InvoiceGenerator/InvoiceGeneratorTest.java
+++ src/main/java/com/InvoiceGenerator/InvoiceGeneratorTest.java
@@ -13,4 +13,21 @@ public class InvoiceGeneratorTest {
         double fare = invoiceGenerator.calculateFare(distance, time);
         Assert.assertEquals(25, fare, 0.0);
     }
+    @Test
+    public void givenLessDistanceAndTime_ShouldReturnMinFare() {
+
+        double distance = 0.1;
+        int time = 1;
+        double fare = invoiceGenerator.calculateFare(distance, time);
+        Assert.assertEquals(5, fare, 0.0);
+    }
+
+    @Test
+    public void givenMultipleRides_ShouldReturnInvoiceSummary() {
+
+        Ride[] rides = {new Ride(2.0, 5),
+                new Ride(0.1, 1)};
+        double totalFare = invoiceGenerator.calculateFare(rides);
+        Assert.assertEquals(30, totalFare, 0.0);
+    }
 }
